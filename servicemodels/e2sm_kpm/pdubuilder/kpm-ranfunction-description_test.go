d7c7e3a682c4a1146a99a66212853f912eae774e
Ivan Eroshkin <erijj@mail.ru>
2020-11-05 13:57:18 +0100

    [SDRAN-36] Adding KpmRanfunctionDescription skeleton message & fixing typo in KpmIndication msg log (#5)

---
+++ servicemodels/e2sm_kpm/pdubuilder/kpm-ranfunction-description_test.go
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package sandbox
+
+import (
+	"gotest.tools/assert"
+	"testing"
+)
+
+func TestE2SmKpmRanfunctionDescriptionMsg(t *testing.T) {
+	var ranFunctionShortName = "ONF"
+	var ranFunctionE2SmOid = "Oid"
+	var ranFunctionDescription = "OpenNetworking"
+	var ranFunctionInstance int32 = 0
+	var ricEventStyleType int32 = 0
+	var ricEventStyleName = "ONFevent"
+	var ricEventFormatType int32 = 0
+	var ricReportStyleType int32 = 0
+	var ricReportStyleName = "ONFreport"
+	var ricIndicationHeaderFormatType int32 = 0
+	var ricIndicationMessageFormatType int32 = 0
+	newE2SmKpmPdu, err := CreateE2SmKpmRanfunctionDescriptionMsg(ranFunctionShortName, ranFunctionE2SmOid, ranFunctionDescription,
+		ranFunctionInstance, ricEventStyleType, ricEventStyleName, ricEventFormatType, ricReportStyleType, ricReportStyleName,
+		ricIndicationHeaderFormatType, ricIndicationMessageFormatType)
+	assert.NilError(t, err)
+	assert.Assert(t, newE2SmKpmPdu != nil)
+}
