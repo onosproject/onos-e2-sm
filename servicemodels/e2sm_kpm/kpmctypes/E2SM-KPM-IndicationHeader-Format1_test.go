69202f9606b57da4087af13c9cb4342540fa183f
Ivan Eroshkin <erijj@mail.ru>
2020-11-17 13:21:03 +0100

    [SDRAN-42] Adding final Go wrappers for E2XmKpmIndicationHeader message (#19)
    * [SDRAN-42] Adding final Go wrappers for E2XmKpmIndicationHeader message
    
    * Final part of E2SmKpmIndicationHeader encoding & decoding

---
+++ servicemodels/e2sm_kpm/kpmctypes/E2SM-KPM-IndicationHeader-Format1_test.go
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package kpmctypes
+
+import (
+	pdubuilder "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm/pdubuilder"
+	"gotest.tools/assert"
+	"testing"
+)
+
+func Test_xerEncodeE2SmKpmIndicationHeaderFormat1(t *testing.T) {
+
+	var plmnID = "ONF"
+	var gNbCuUpId int64 = 0
+	var gNbDuId int64 = 0
+	var plmnIDnrcgi = "onf"
+	var sst = "1"
+	var sd = "SD1"
+	var fiveQi int32 = 0
+	var qCi int32 = 0
+
+	newE2SmKpmPdu, err := pdubuilder.CreateE2SmKpmIndicationHeader(plmnID, gNbCuUpId, gNbDuId, plmnIDnrcgi, sst, sd, fiveQi, qCi)
+
+	xer, err := xerEncodeE2SmKpmIndicationHeaderFormat1(newE2SmKpmPdu.GetIndicationHeaderFormat1())
+	assert.NilError(t, err)
+	t.Logf("E2SM-KPM-IndicationHeader XER\n%s", string(xer))
+}
