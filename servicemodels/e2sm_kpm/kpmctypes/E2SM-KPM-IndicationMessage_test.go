43aba155b198e8ea385d18ad3046f2eb2343e890
eroshiva <erijj@mail.ru>
2020-11-17 14:52:51 +0100

    Reverting changes in IndicationMessage_test - too early to use pdubuilder

--- servicemodels/e2sm_kpm/kpmctypes/E2SM-KPM-IndicationMessage_test.go
+++ servicemodels/e2sm_kpm/kpmctypes/E2SM-KPM-IndicationMessage_test.go
@@ -5,34 +5,45 @@
 package kpmctypes
 
 import (
-	pdubuilder "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm/pdubuilder"
+	e2sm_kpm_ies "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm/v1beta1/e2sm-kpm-ies"
 	"gotest.tools/assert"
 	"testing"
 )
 
 func Test_xerEncodeE2SmKpmIndicationMessage(t *testing.T) {
 
-	var plmnID = "ONF"
-	var cellIdentityValue uint64 = 0
-	var cellIdentityLen uint32 = 0
-	var ulTotalAvlblProbs int32 = 0
-	var dlTotalAvlblProbs int32 = 0
-	var fiveQi int32 = 0
-	var dlPrbusage int32 = 0
-	var ulPrbusage int32 = 0
-	var qCi int32 = 0
-	var qciDlPrbusage int32 = 0
-	var qciUlPrbusage int32 = 0
-	var sst = "1"
-	var sd = "SD1"
-	var gNbCuName = "OpenNetworking"
-	var cuCpNumberActvts int32 = 0
-	var ranContainer = "ranContainer"
-	newE2SmKpmPdu, err := pdubuilder.CreateE2SmKpmIndicationMsg(plmnID, cellIdentityValue, cellIdentityLen, dlTotalAvlblProbs,
-		ulTotalAvlblProbs, fiveQi, dlPrbusage, ulPrbusage, qCi, qciDlPrbusage, qciUlPrbusage, sst,
-		sd, gNbCuName, cuCpNumberActvts, ranContainer)
+	var testGnbCuCpName = "0xAB"
+	var testNumberOfActiveUes int32 = 1
 
-	xer, err := xerEncodeE2SmKpmIndicationMessage(newE2SmKpmPdu)
+	containerOcuCp1 := &e2sm_kpm_ies.PmContainersList{
+		PerformanceContainer: &e2sm_kpm_ies.PfContainer{
+			PfContainer: &e2sm_kpm_ies.PfContainer_OCuCp{
+				OCuCp: &e2sm_kpm_ies.OcucpPfContainer{
+					GNbCuCpName: &e2sm_kpm_ies.GnbCuCpName{
+						Value: testGnbCuCpName, //string -- PrintableString(SIZE(1..150,...))
+					},
+					CuCpResourceStatus: &e2sm_kpm_ies.OcucpPfContainer_CuCpResourceStatus001{
+						NumberOfActiveUes: testNumberOfActiveUes, //int32 -- INTEGER (1..65536, ...)
+					},
+				},
+				//TODO: Implement other types of containers
+			},
+		},
+		//TODO: Implement TheRancontainer
+	}
+
+	e2SmIindicationMsg := &e2sm_kpm_ies.E2SmKpmIndicationMessage_IndicationMessageFormat1{
+		IndicationMessageFormat1: &e2sm_kpm_ies.E2SmKpmIndicationMessageFormat1{
+			PmContainers: make([]*e2sm_kpm_ies.PmContainersList, 0),
+		},
+	}
+	e2SmIindicationMsg.IndicationMessageFormat1.PmContainers = append(e2SmIindicationMsg.IndicationMessageFormat1.PmContainers, containerOcuCp1)
+
+	e2smKpmPdu := &e2sm_kpm_ies.E2SmKpmIndicationMessage{
+		E2SmKpmIndicationMessage: e2SmIindicationMsg,
+	}
+
+	xer, err := xerEncodeE2SmKpmIndicationMessage(e2smKpmPdu)
 	assert.NilError(t, err)
 	t.Logf("E2SM-KPM-IndicationMessage XER\n%s", string(xer))
 }
