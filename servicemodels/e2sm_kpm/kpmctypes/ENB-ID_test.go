4311a4f5fe0f0e148a85e1f2c48b9084cc16a0aa
eroshiva <erijj@mail.ru>
2020-12-11 11:25:19 +0100

    Adding ENbID structures(decode/encode)

---
+++ servicemodels/e2sm_kpm/kpmctypes/ENB-ID_test.go
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package kpmctypes
+
+import (
+	e2sm_kpm_ies "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm/v1beta1/e2sm-kpm-ies"
+	"gotest.tools/assert"
+	"testing"
+)
+
+func Test_xerEncodeENbID(t *testing.T) {
+
+	enbID := &e2sm_kpm_ies.EnbId{
+		EnbId: &e2sm_kpm_ies.EnbId_HomeENbId{
+			HomeENbId: &e2sm_kpm_ies.BitString{
+				Value: 0x9bcd4, //uint64
+				Len:   22,      //uint32
+			},
+		},
+	}
+
+	xer, err := xerEncodeENbID(enbID)
+	assert.NilError(t, err)
+	t.Logf("NRCGI XER\n%s", string(xer))
+}
