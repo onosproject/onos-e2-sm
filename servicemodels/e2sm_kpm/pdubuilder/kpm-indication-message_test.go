e8bfd7a2a8bc458acb78452dde55dbe4b7554355
Sean Condon <sean@opennetworking.org>
2020-12-17 11:01:00 +0000

    Updated README. Added more tests

--- servicemodels/e2sm_kpm/pdubuilder/kpm-indication-message_test.go
+++ servicemodels/e2sm_kpm/pdubuilder/kpm-indication-message_test.go
@@ -5,6 +5,7 @@
 package pdubuilder
 
 import (
+	"github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm/kpmctypes"
 	"gotest.tools/assert"
 	"testing"
 )
@@ -24,11 +25,20 @@ func TestE2SmKpmIndicationMsg(t *testing.T) {
 	var sst = "1"
 	var sd = "SD1"
 	var gNbCuName = "OpenNetworking"
-	var cuCpNumberActvts int32 = 0
+	var cuCpNumberActvts int32 = 1
 	var ranContainer = "ranContainer"
 	newE2SmKpmPdu, err := CreateE2SmKpmIndicationMsg(plmnID, cellIdentityValue, cellIdentityLen, dlTotalAvlblProbs,
 		ulTotalAvlblProbs, fiveQi, dlPrbusage, ulPrbusage, qCi, qciDlPrbusage, qciUlPrbusage, sst,
 		sd, gNbCuName, cuCpNumberActvts, ranContainer)
 	assert.NilError(t, err)
 	assert.Assert(t, newE2SmKpmPdu != nil)
+
+	xer, err := kpmctypes.XerEncodeE2SmKpmIndicationMessage(newE2SmKpmPdu)
+	assert.NilError(t, err)
+	t.Logf("XER Encoded Ind Message: %s", string(xer))
+
+	per, err := kpmctypes.PerEncodeE2SmKpmIndicationMessage(newE2SmKpmPdu)
+	assert.NilError(t, err)
+	t.Logf("PER Encoded Ind Message: % x", per)
+
 }
