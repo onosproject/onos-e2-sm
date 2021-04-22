fee1be61e0ed518838a96a8809bad0dd3fd29cbb
Ivan Eroshkin <erijj@mail.ru>
2021-04-22 11:33:43 +0200

    RC-PRE SM - extending unit test for RanParameterValue (#150)

--- servicemodels/e2sm_rc_pre/rcprectypes/RANparameter-Value_test.go
+++ servicemodels/e2sm_rc_pre/rcprectypes/RANparameter-Value_test.go
@@ -5,6 +5,7 @@
 package rcprectypes
 
 import (
+	"encoding/hex"
 	e2sm_rc_pre_ies "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_rc_pre/v1/e2sm-rc-pre-ies"
 	"gotest.tools/assert"
 	"testing"
@@ -26,32 +27,40 @@ func createRanparameterValueValueEnum() *e2sm_rc_pre_ies.RanparameterValue {
 	}
 }
 
-//func createRanparameterValueValueOctS() *e2sm_rc_pre_ies.RanparameterValue {
-//	return &e2sm_rc_pre_ies.RanparameterValue{
-//		RanparameterValue: &e2sm_rc_pre_ies.RanparameterValue_ValueOctS{
-//			ValueOctS: "onf",
-//		},
-//	}
-//}
-//
-//func createRanparameterValueValuePrtS() *e2sm_rc_pre_ies.RanparameterValue {
-//	return &e2sm_rc_pre_ies.RanparameterValue{
-//		RanparameterValue: &e2sm_rc_pre_ies.RanparameterValue_ValuePrtS{
-//			ValuePrtS: "onf",
-//		},
-//	}
-//}
-//
-//func createRanparameterValueValueBitS() *e2sm_rc_pre_ies.RanparameterValue {
-//	return &e2sm_rc_pre_ies.RanparameterValue{
-//		RanparameterValue: &e2sm_rc_pre_ies.RanparameterValue_ValueBitS{
-//			ValueBitS: &e2sm_rc_pre_ies.BitString{
-//				Value: 0x9bcd4,
-//				Len:   22,
-//			},
-//		},
-//	}
-//}
+func createRanparameterValueValueBool() *e2sm_rc_pre_ies.RanparameterValue {
+	return &e2sm_rc_pre_ies.RanparameterValue{
+		RanparameterValue: &e2sm_rc_pre_ies.RanparameterValue_ValueBool{
+			ValueBool: true,
+		},
+	}
+}
+
+func createRanparameterValueValueOctS() *e2sm_rc_pre_ies.RanparameterValue {
+	return &e2sm_rc_pre_ies.RanparameterValue{
+		RanparameterValue: &e2sm_rc_pre_ies.RanparameterValue_ValueOctS{
+			ValueOctS: "onf",
+		},
+	}
+}
+
+func createRanparameterValueValuePrtS() *e2sm_rc_pre_ies.RanparameterValue {
+	return &e2sm_rc_pre_ies.RanparameterValue{
+		RanparameterValue: &e2sm_rc_pre_ies.RanparameterValue_ValuePrtS{
+			ValuePrtS: "onf",
+		},
+	}
+}
+
+func createRanparameterValueValueBitS() *e2sm_rc_pre_ies.RanparameterValue {
+	return &e2sm_rc_pre_ies.RanparameterValue{
+		RanparameterValue: &e2sm_rc_pre_ies.RanparameterValue_ValueBitS{
+			ValueBitS: &e2sm_rc_pre_ies.BitString{
+				Value: 0x9bcd4,
+				Len:   22,
+			},
+		},
+	}
+}
 
 func Test_xerEncodeRanparameterValue(t *testing.T) {
 
@@ -69,26 +78,33 @@ func Test_xerEncodeRanparameterValue(t *testing.T) {
 	assert.Equal(t, 73, len(xer))
 	t.Logf("RANparameterValue (Enum) XER\n%s", string(xer))
 
-	//rpv = createRanparameterValueValueOctS()
-	//
-	//xer, err = xerEncodeRanparameterValue(rpv)
-	//assert.NilError(t, err)
-	//assert.Equal(t, 71, len(xer))
-	//t.Logf("RANparameterValue (OctetString) XER\n%s", string(xer))
-	//
-	//rpv = createRanparameterValueValuePrtS()
-	//
-	//xer, err = xerEncodeRanparameterValue(rpv)
-	//assert.NilError(t, err)
-	//assert.Equal(t, 71, len(xer))
-	//t.Logf("RANparameterValue (PrintableString) XER\n%s", string(xer))
+	rpv = createRanparameterValueValueBool()
 
-	//rpv = createRanparameterValueValueBitS()
-	//
-	//xer, err = xerEncodeRanparameterValue(rpv)
-	//assert.NilError(t, err)
-	//assert.Equal(t, 71, len(xer))
-	//t.Logf("RANparameterValue (BitString) XER\n%s", string(xer))
+	xer, err = xerEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 78, len(xer))
+	t.Logf("RANparameterValue (Bool) XER\n%s", string(xer))
+
+	rpv = createRanparameterValueValueOctS()
+
+	xer, err = xerEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 79, len(xer))
+	t.Logf("RANparameterValue (OctetString) XER\n%s", string(xer))
+
+	rpv = createRanparameterValueValuePrtS()
+
+	xer, err = xerEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 74, len(xer))
+	t.Logf("RANparameterValue (PrintableString) XER\n%s", string(xer))
+
+	rpv = createRanparameterValueValueBitS()
+
+	xer, err = xerEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 107, len(xer))
+	t.Logf("RANparameterValue (BitString) XER\n%s", string(xer))
 }
 
 func Test_xerDecodeMeasurementRecordItem(t *testing.T) {
@@ -117,40 +133,53 @@ func Test_xerDecodeMeasurementRecordItem(t *testing.T) {
 	assert.Assert(t, result != nil)
 	t.Logf("RANparameterValue (Enum) XER - decoded\n%s", result)
 
-	//rpv = createRanparameterValueValueOctS()
-	//
-	//xer, err = xerEncodeRanparameterValue(rpv)
-	//assert.NilError(t, err)
-	//assert.Equal(t, 71, len(xer))
-	//t.Logf("RANparameterValue (OctetString) XER\n%s", string(xer))
-	//
-	//result, err = xerDecodeRanparameterValue(xer)
-	//assert.NilError(t, err)
-	//assert.Assert(t, result != nil)
-	//t.Logf("RANparameterValue (OctetString) XER - decoded\n%s", result)
-	//
-	//rpv = createRanparameterValueValuePrtS()
-	//
-	//xer, err = xerEncodeRanparameterValue(rpv)
-	//assert.NilError(t, err)
-	//assert.Equal(t, 71, len(xer))
-	//t.Logf("RANparameterValue (PrintableString) XER\n%s", string(xer))
-	//
-	//result, err = xerDecodeRanparameterValue(xer)
-	//assert.NilError(t, err)
-	//assert.Assert(t, result != nil)
-	//t.Logf("RANparameterValue (PrintableString) XER - decoded\n%s", result)
+	rpv = createRanparameterValueValueBool()
 
-	//rpv = createRanparameterValueValueBitS()
-	//
-	//xer, err = xerEncodeRanparameterValue(rpv)
-	//assert.NilError(t, err)
-	//assert.Equal(t, 71, len(xer))
-	//t.Logf("RANparameterValue (BitString) XER\n%s", string(xer))
-	//
-	//result, err = xerDecodeRanparameterValue(xer)
-	//assert.NilError(t, err)
-	//assert.Assert(t, result != nil)
+	xer, err = xerEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 78, len(xer))
+	t.Logf("RANparameterValue (Bool) XER\n%s", string(xer))
+
+	result, err = xerDecodeRanparameterValue(xer)
+	assert.NilError(t, err)
+	assert.Assert(t, result != nil)
+	t.Logf("RANparameterValue (Bool) XER - decoded\n%s", result)
+
+	rpv = createRanparameterValueValueOctS()
+
+	xer, err = xerEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 79, len(xer))
+	t.Logf("RANparameterValue (OctetString) XER\n%s", string(xer))
+
+	result, err = xerDecodeRanparameterValue(xer)
+	assert.NilError(t, err)
+	assert.Assert(t, result != nil)
+	t.Logf("RANparameterValue (OctetString) XER - decoded\n%s", result)
+
+	rpv = createRanparameterValueValuePrtS()
+
+	xer, err = xerEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 74, len(xer))
+	t.Logf("RANparameterValue (PrintableString) XER\n%s", string(xer))
+
+	result, err = xerDecodeRanparameterValue(xer)
+	assert.NilError(t, err)
+	assert.Assert(t, result != nil)
+	t.Logf("RANparameterValue (PrintableString) XER - decoded\n%s", result)
+
+	rpv = createRanparameterValueValueBitS()
+
+	xer, err = xerEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 107, len(xer))
+	t.Logf("RANparameterValue (BitString) XER\n%s", string(xer))
+
+	result, err = xerDecodeRanparameterValue(xer)
+	assert.NilError(t, err)
+	assert.Assert(t, result != nil)
+	t.Logf("RANparameterValue (BitString) XER - decoded\n%s", result)
 }
 
 func Test_perEncodeMeasurementRecordItem(t *testing.T) {
@@ -160,35 +189,42 @@ func Test_perEncodeMeasurementRecordItem(t *testing.T) {
 	per, err := perEncodeRanparameterValue(rpv)
 	assert.NilError(t, err)
 	assert.Equal(t, 3, len(per))
-	t.Logf("RANparameterValue (Integer) PER\n%s", string(per))
+	t.Logf("RANparameterValue (Integer) PER\n%s", hex.Dump(per))
 
 	rpv = createRanparameterValueValueEnum()
 
 	per, err = perEncodeRanparameterValue(rpv)
 	assert.NilError(t, err)
 	assert.Equal(t, 3, len(per))
-	t.Logf("RANparameterValue (Enum) PER\n%s", string(per))
+	t.Logf("RANparameterValue (Enum) PER\n%s", hex.Dump(per))
 
-	//rpv = createRanparameterValueValueOctS()
-	//
-	//per, err = perEncodeRanparameterValue(rpv)
-	//assert.NilError(t, err)
-	//assert.Equal(t, 2, len(per))
-	//t.Logf("RANparameterValue (OctetString) PER\n%s", string(per))
-	//
-	//rpv = createRanparameterValueValuePrtS()
-	//
-	//per, err = perEncodeRanparameterValue(rpv)
-	//assert.NilError(t, err)
-	//assert.Equal(t, 2, len(per))
-	//t.Logf("RANparameterValue (PrintableString) PER\n%s", string(per))
+	rpv = createRanparameterValueValueBool()
 
-	//rpv = createRanparameterValueValueBitS()
-	//
-	//per, err = perEncodeRanparameterValue(rpv)
-	//assert.NilError(t, err)
-	//assert.Equal(t, 4, len(per))
-	//t.Logf("RANparameterValue (BitString) PER\n%s", string(per))
+	per, err = perEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 1, len(per))
+	t.Logf("RANparameterValue (Bool) PER\n%s", hex.Dump(per))
+
+	rpv = createRanparameterValueValueOctS()
+
+	per, err = perEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 5, len(per))
+	t.Logf("RANparameterValue (OctetString) PER\n%s", hex.Dump(per))
+
+	rpv = createRanparameterValueValuePrtS()
+
+	per, err = perEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 5, len(per))
+	t.Logf("RANparameterValue (PrintableString) PER\n%s", hex.Dump(per))
+
+	rpv = createRanparameterValueValueBitS()
+
+	per, err = perEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 5, len(per))
+	t.Logf("RANparameterValue (BitString) PER\n%s", hex.Dump(per))
 }
 
 func Test_perDecodeMeasurementRecordItem(t *testing.T) {
@@ -198,7 +234,7 @@ func Test_perDecodeMeasurementRecordItem(t *testing.T) {
 	per, err := perEncodeRanparameterValue(rpv)
 	assert.NilError(t, err)
 	assert.Equal(t, 3, len(per))
-	t.Logf("RANparameterValue (Integer) PER\n%s", string(per))
+	t.Logf("RANparameterValue (Integer) PER\n%s", hex.Dump(per))
 
 	result, err := perDecodeRanparameterValue(per)
 	assert.NilError(t, err)
@@ -210,43 +246,58 @@ func Test_perDecodeMeasurementRecordItem(t *testing.T) {
 	per, err = perEncodeRanparameterValue(rpv)
 	assert.NilError(t, err)
 	assert.Equal(t, 3, len(per))
-	t.Logf("RANparameterValue (Enum) PER\n%s", string(per))
+	t.Logf("RANparameterValue (Enum) PER\n%s", hex.Dump(per))
 
 	result, err = perDecodeRanparameterValue(per)
 	assert.NilError(t, err)
 	assert.Assert(t, result != nil)
 	t.Logf("RANparameterValue (Enum) PER - decoded\n%s", result)
 
-	//rpv = createRanparameterValueValueOctS()
-	//
-	//per, err = perEncodeRanparameterValue(rpv)
-	//assert.NilError(t, err)
-	//assert.Equal(t, 2, len(per))
-	//t.Logf("RANparameterValue (OctetString) PER\n%s", string(per))
-	//
-	//result, err = perDecodeRanparameterValue(per)
-	//assert.NilError(t, err)
-	//assert.Assert(t, result != nil)
-	//
-	//rpv = createRanparameterValueValuePrtS()
-	//
-	//per, err = perEncodeRanparameterValue(rpv)
-	//assert.NilError(t, err)
-	//assert.Equal(t, 2, len(per))
-	//t.Logf("RANparameterValue (PrintableString) PER\n%s", string(per))
-	//
-	//result, err = perDecodeRanparameterValue(per)
-	//assert.NilError(t, err)
-	//assert.Assert(t, result != nil)
+	rpv = createRanparameterValueValueBool()
 
-	//rpv = createRanparameterValueValueBitS()
-	//
-	//per, err = perEncodeRanparameterValue(rpv)
-	//assert.NilError(t, err)
-	//assert.Equal(t, 4, len(per))
-	//t.Logf("RANparameterValue (BitString) PER\n%s", string(per))
-	//
-	//result, err = perDecodeRanparameterValue(per)
-	//assert.NilError(t, err)
-	//assert.Assert(t, result != nil)
+	per, err = perEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 1, len(per))
+	t.Logf("RANparameterValue (Bool) PER\n%s", hex.Dump(per))
+
+	result, err = perDecodeRanparameterValue(per)
+	assert.NilError(t, err)
+	assert.Assert(t, result != nil)
+	t.Logf("RANparameterValue (Bool) PER - decoded\n%s", result)
+
+	rpv = createRanparameterValueValueOctS()
+
+	per, err = perEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 5, len(per))
+	t.Logf("RANparameterValue (OctetString) PER\n%s", hex.Dump(per))
+
+	result, err = perDecodeRanparameterValue(per)
+	assert.NilError(t, err)
+	assert.Assert(t, result != nil)
+	t.Logf("RANparameterValue (OctetString) PER - decoded\n%s", result)
+
+	rpv = createRanparameterValueValuePrtS()
+
+	per, err = perEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 5, len(per))
+	t.Logf("RANparameterValue (PrintableString) PER\n%s", hex.Dump(per))
+
+	result, err = perDecodeRanparameterValue(per)
+	assert.NilError(t, err)
+	assert.Assert(t, result != nil)
+	t.Logf("RANparameterValue (PrintableString) PER - decoded\n%s", result)
+
+	rpv = createRanparameterValueValueBitS()
+
+	per, err = perEncodeRanparameterValue(rpv)
+	assert.NilError(t, err)
+	assert.Equal(t, 5, len(per))
+	t.Logf("RANparameterValue (BitString) PER\n%s", hex.Dump(per))
+
+	result, err = perDecodeRanparameterValue(per)
+	assert.NilError(t, err)
+	assert.Assert(t, result != nil)
+	t.Logf("RANparameterValue (BitString) PER - decoded\n%s", result)
 }
