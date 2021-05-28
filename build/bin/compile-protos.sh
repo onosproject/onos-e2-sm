d92a4fda2e90a65ef742db63a279f5f6ea327a01
Sean Condon <sean@opennetworking.org>
2021-05-28 08:30:36 +0100

    Update Makefile to use new protoc

--- build/bin/compile-protos.sh
+++ build/bin/compile-protos.sh
@@ -10,6 +10,13 @@ protoc -I=$proto_imports --validate_out=lang=go:. --proto_path=servicemodels \
   --go_out=. \
   e2sm_kpm_v2/v2/e2sm_kpm_v2.proto
 
+#For future modules based on onos-lib-go/api/asn1 BitString
+#protoc -I=$proto_imports:${GOPATH}/src/github.com/onosproject/onos-lib-go/api \
+#  --validate_out=lang=go:. --proto_path=servicemodels \
+#  --go_out=../../.. \
+#  e2sm_kpm_v2/v2/e2sm_kpm_v2.proto
+#protoc-go-inject-tag -input=servicemodels/e2sm_kpm_v2/v2/e2sm-kpm-v2/e2sm_kpm_v2.pb.go
+
 #protoc -I=$proto_imports --validate_out=lang=go:. --proto_path=servicemodels \
 #  --go_out=. \
 #  e2sm_ni/v1beta1/e2sm_ni_ies.proto
