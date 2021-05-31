fc44210c0423b28d31092d36774a7fc951f628f7
Ivan Eroshkin <erijj@mail.ru>
2021-05-31 14:48:30 +0200

    KPMv2 Go APER

--- build/bin/compile-protos.sh
+++ build/bin/compile-protos.sh
@@ -17,6 +17,12 @@ protoc -I=$proto_imports --validate_out=lang=go:. --proto_path=servicemodels \
 #  e2sm_kpm_v2/v2/e2sm_kpm_v2.proto
 #protoc-go-inject-tag -input=servicemodels/e2sm_kpm_v2/v2/e2sm-kpm-v2/e2sm_kpm_v2.pb.go
 
+protoc -I=$proto_imports:${GOPATH}/src/github.com/onosproject/onos-lib-go/api \
+  --proto_path=servicemodels \
+  --go_out=./servicemodels/ \
+  e2sm_kpm_v2_go/v2/e2sm_kpm_v2_go.proto
+protoc-go-inject-tag -input=servicemodels/e2sm_kpm_v2_go/v2/e2sm-kpm-v2-go/e2sm_kpm_v2_go.pb.go
+
 #protoc -I=$proto_imports --validate_out=lang=go:. --proto_path=servicemodels \
 #  --go_out=. \
 #  e2sm_ni/v1beta1/e2sm_ni_ies.proto
---
+++ servicemodels/e2sm_kpm_v2_go/go.mod
@@ -0,0 +1,5 @@
+module github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm_v2_go
+
+go 1.16
+
+require github.com/onosproject/onos-lib-go v0.7.13 // indirect
