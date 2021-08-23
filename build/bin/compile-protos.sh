877e5f7bcd58b18195131414f126610d484030f1
Ivan Eroshkin <erijj@mail.ru>
2021-08-23 17:50:06 +0200

    [SDRAN-1074,1026] Introducing Protobuf for RSM and E2-SM

--- build/bin/compile-protos.sh
+++ build/bin/compile-protos.sh
@@ -23,6 +23,18 @@ protoc -I=$proto_imports:${GOPATH}/src/github.com/onosproject/onos-lib-go/api \
   e2sm_kpm_v2_go/v2/e2sm_kpm_v2_go.proto
 protoc-go-inject-tag -input=servicemodels/e2sm_kpm_v2_go/v2/e2sm-kpm-v2-go/e2sm_kpm_v2_go.pb.go
 
+protoc -I=$proto_imports:${GOPATH}/src/github.com/onosproject/onos-lib-go/api \
+  --proto_path=servicemodels \
+  --go_out=./servicemodels/ \
+  e2sm_rsm/v1/e2sm_rsm_v1.proto
+protoc-go-inject-tag -input=servicemodels/e2sm_rsm/v1/e2sm-rsm-ies/e2sm_rsm_v1.pb.go
+
+protoc -I=$proto_imports:${GOPATH}/src/github.com/onosproject/onos-lib-go/api \
+  --proto_path=servicemodels \
+  --go_out=./servicemodels/ \
+  e2sm_rsm/v1/e2sm_v2.proto
+protoc-go-inject-tag -input=servicemodels/e2sm_rsm/v1/e2sm-v2-ies/e2sm_v2.pb.go
+
 #protoc -I=$proto_imports --validate_out=lang=go:. --proto_path=servicemodels \
 #  --go_out=. \
 #  e2sm_ni/v1beta1/e2sm_ni_ies.proto
