31ff96386ac25a600c1ed766eeb29b479cac4477
Ivan Eroshkin <erijj@mail.ru>
2021-09-29 15:29:23 +0200

    [SDRAN-1166] RSM fix (E2AP v1.01)

--- servicemodels/e2sm_rsm/pdubuilder/E2SM-RSM-EventTriggerDefinition_test.go
+++ servicemodels/e2sm_rsm/pdubuilder/E2SM-RSM-EventTriggerDefinition_test.go
@@ -7,6 +7,7 @@ package pdubuilder
 import (
 	"encoding/hex"
 	"github.com/onosproject/onos-e2-sm/servicemodels/e2sm_rsm/encoder"
+	e2sm_rsm_ies "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_rsm/v1/e2sm-rsm-ies"
 	"gotest.tools/assert"
 	"testing"
 )
@@ -27,4 +28,14 @@ func Test_E2SmRsmEventTriggerDefinition(t *testing.T) {
 	assert.Assert(t, result != nil)
 	t.Logf("E2SM-RSM-EventTriggerDefinition PER - decoded\n%v", result)
 	assert.DeepEqual(t, etd.String(), result.String())
+}
+
+func Test_Woojong(t *testing.T) {
+	per := []byte{0x00}
+
+	result, err := encoder.PerDecodeE2SmRsmEventTriggerDefinition(per)
+	assert.NilError(t, err)
+	assert.Assert(t, result != nil)
+	t.Logf("E2SM-RSM-EventTriggerDefinition PER - decoded\n%v", result)
+	assert.Equal(t, result.GetEventDefinitionFormats().GetEventDefinitionFormat1().GetTriggerType().Number(), e2sm_rsm_ies.RsmRicindicationTriggerType_RSM_RICINDICATION_TRIGGER_TYPE_PERIODIC_METRICS.Number())
 }
