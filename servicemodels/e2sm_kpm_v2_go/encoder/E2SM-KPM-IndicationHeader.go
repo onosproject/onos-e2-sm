831e179a5289fb88692eb440254469d3ff39a1a9
Ivan Eroshkin <erijj@mail.ru>
2022-02-15 17:29:00 +0100

    nil

--- servicemodels/e2sm_kpm_v2_go/encoder/E2SM-KPM-IndicationHeader.go
+++ servicemodels/e2sm_kpm_v2_go/encoder/E2SM-KPM-IndicationHeader.go
@@ -6,7 +6,6 @@ package encoder
 
 import (
 	"encoding/hex"
-	"github.com/google/martian/log"
 	e2sm_kpm_v2_go "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm_v2_go/v2/e2sm-kpm-v2-go"
 	"github.com/onosproject/onos-lib-go/pkg/asn1/aper"
 )
